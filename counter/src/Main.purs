d8bf4c3f71b064bef063abd07d9548e3833ad15c
Ray Shih <mnf.shih@gmail.com>
2017-05-24 22:42:56 +0800

    basic counter

--- counter/src/Main.purs
+++ counter/src/Main.purs
@@ -1,9 +1,37 @@
 module Main where
 
-import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Prelude hiding (div)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+import Control.Monad.Eff (Eff)
+import Pux (CoreEffects, EffModel, start)
+import Pux.Renderer.React (renderToDOM)
+import Pux.DOM.HTML (HTML)
+import Pux.DOM.Events (onClick)
+import Text.Smolder.HTML (div, button)
+import Text.Smolder.Markup (text, (#!))
+
+data Event = Increment | Decrement
+
+type State = Int
+
+view :: State -> HTML Event
+view count =
+  div do
+    button #! onClick (const Increment) $ text "Increment"
+    text $ show count
+    button #! onClick (const Decrement) $ text "Decrement"
+
+foldp :: forall fx. Event -> State -> EffModel State Event fx
+foldp Increment state = { state: state + 1, effects: [] }
+foldp Decrement state = { state: state - 1, effects: [] }
+
+main :: forall fx. Eff (CoreEffects fx) Unit
 main = do
-  log "Hello sailor!"
+  app <- start
+    { initialState: 0
+    , view
+    , foldp
+    , inputs: []
+    }
+
+  renderToDOM "#app" app.markup app.input
